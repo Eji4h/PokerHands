81302cc871f393b9db1cac6092157f24aa5fa1a8
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-05-25 02:34:53 +0700

    Remove unfavourable

--- PokerHandsTest/DeckTest.cs
+++ PokerHandsTest/DeckTest.cs
@@ -15,23 +15,5 @@ namespace PokerHandsTest
         {
             Assert.AreEqual(52, deck.Cards.Length);
         }
-
-        [Test]
-        public void FirstCardOfDeck_ShouldBe_AceClub()
-        {
-            Card aceClubCard = new Card(SuitType.Club, RankType.Ace);
-            Card firstCardOfDeck = deck.Cards[0];
-            Assert.AreEqual(aceClubCard.Suit, firstCardOfDeck.Suit);
-            Assert.AreEqual(aceClubCard.Rank, firstCardOfDeck.Rank);
-        }
-
-        [Test]
-        public void LastCardOfDeck_ShouldBe_KingSpade()
-        {
-            Card kingSpadeCard = new Card(SuitType.Spade, RankType.King);
-            Card lastCardOfDeck = deck.Cards[deck.Cards.Length - 1];
-            Assert.AreEqual(kingSpadeCard.Suit, lastCardOfDeck.Suit);
-            Assert.AreEqual(kingSpadeCard.Rank, lastCardOfDeck.Rank);
-        }
     }
 }
