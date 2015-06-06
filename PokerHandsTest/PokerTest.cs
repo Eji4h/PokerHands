e1a6661d6c3cb056b85d0943a17680634220c333
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-06-06 23:45:46 +0700

    Refactor

--- PokerHandsTest/PokerTest.cs
+++ PokerHandsTest/PokerTest.cs
@@ -11,9 +11,6 @@ namespace PokerHandsTest
     [TestFixture]
     public class PokerTest
     {
-        SuitType dummyCardSuit = SuitType.Club;
-        RankType dummyCardRank = RankType.Two;
-
         Card dummyCardRank2 = new Card(SuitType.Club, RankType.Two);
         Card dummyCardRank3 = new Card(SuitType.Club, RankType.Three);
         Card dummyCardRank4 = new Card(SuitType.Club, RankType.Four);
@@ -27,8 +24,6 @@ namespace PokerHandsTest
         Card dummyCardRankQueen = new Card(SuitType.Club, RankType.Queen);
         Card dummyCardRankKing = new Card(SuitType.Club, RankType.King);
         Card dummyCardRankAce = new Card(SuitType.Club, RankType.Ace);
-
-        Card dummyCardRank2Diamond = new Card(SuitType.Diamond, RankType.Two);
 
         List<Card> onHandCards23456Dummy;
         List<Card> onHandCards34567Dummy;
@@ -51,97 +46,91 @@ namespace PokerHandsTest
 
             onHandCards22345Dummy = new List<Card>()
             {
-                dummyCardRank2, dummyCardRank2Diamond,
+                dummyCardRank2, dummyCardRank2,
                 dummyCardRank3, dummyCardRank4, dummyCardRank5
             };
 
             onHandCards23345Dummy = new List<Card>()
             {
                 dummyCardRank2, 
-                dummyCardRank3, new Card(SuitType.Diamond, RankType.Three),
+                dummyCardRank3, dummyCardRank3,
                 dummyCardRank4, dummyCardRank5
             };
         }
+
+        #region RecognizeCatagory
+        [Test]
+        public void OnHandIs_22345_CategoryShouldBe_Pair()
+        {
+            Assert.AreEqual(Poker.Category.Pair, Poker.RecognizeCategory(onHandCards22345Dummy));
+        }
+
+        [Test]
+        public void OnHandIs_23456_CategoryShouldBe_HighCard()
+        {
+            Assert.AreEqual(Poker.Category.HighCard, Poker.RecognizeCategory(onHandCards23456Dummy));
+        }
+        #endregion
 
         #region CompareScoring
         [Test]
         public void CompareScoring_2Vs3_ResultDual_ShouldBe_Lose()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Two);
-            Card card2 = new Card(dummyCardSuit, RankType.Three);
-            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(dummyCardRank2, dummyCardRank3));
         }
 
         [Test]
         public void CompareScoring_3Vs2_ResultDual_ShouldBe_Win()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Three);
-            Card card2 = new Card(dummyCardSuit, RankType.Two);
-            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(dummyCardRank3, dummyCardRank2));
         }
 
         [Test]
         public void CompareScoring_2VsJack_ResultDual_ShouldBe_Lose()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Two);
-            Card card2 = new Card(dummyCardSuit, RankType.Jack);
-            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(dummyCardRank2, dummyCardRankJack));
         }
 
         [Test]
         public void CompareScoring_2Vs2_ResultDual_ShouldBe_Draw()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Two);
-            Card card2 = new Card(dummyCardSuit, RankType.Two);
-            Assert.AreEqual(ResultDual.Draw, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Draw, Poker.CompareScoring(dummyCardRank2, dummyCardRank2));
         }
 
         [Test]
         public void CompareScoring_2VsAce_ResultDual_ShouldBe_Lose()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Two);
-            Card card2 = new Card(dummyCardSuit, RankType.Ace);
-            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(dummyCardRank2, dummyCardRankAce));
         }
 
         [Test]
         public void CompareScoring_AceVs2_ResultDual_ShouldBe_Win()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Ace);
-            Card card2 = new Card(dummyCardSuit, RankType.Two);
-            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(dummyCardRankAce, dummyCardRank2));
         }
 
         [Test]
         public void CompareScoring_AceVsKing_ResultDual_ShouldBe_Win()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Ace);
-            Card card2 = new Card(dummyCardSuit, RankType.King);
-            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(dummyCardRankAce, dummyCardRankKing));
         }
 
         [Test]
         public void CompareScoring_QueenVsAce_ResultDual_ShouldBe_Lose()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Queen);
-            Card card2 = new Card(dummyCardSuit, RankType.Ace);
-            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(dummyCardRankQueen, dummyCardRankAce));
         }
 
         [Test]
         public void CompareScoring_5VsKing_ResultDual_ShouldBe_Lose()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.Five);
-            Card card2 = new Card(dummyCardSuit, RankType.King);
-            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Lose, Poker.CompareScoring(dummyCardRank5, dummyCardRankKing));
         }
 
         [Test]
         public void CompareScoring_KingVs5_ResultDual_ShouldBe_Win()
         {
-            Card card1 = new Card(dummyCardSuit, RankType.King);
-            Card card2 = new Card(dummyCardSuit, RankType.Five);
-            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(card1, card2));
+            Assert.AreEqual(ResultDual.Win, Poker.CompareScoring(dummyCardRankKing, dummyCardRank5));
         }
         #endregion
 
@@ -149,7 +138,8 @@ namespace PokerHandsTest
         [Test]
         public void CompareHighCard_HandOneIs_23456_And_HandTwoIs_34567_ShouldBe_Lose()
         {
-            Assert.AreEqual(ResultDual.Lose, Poker.CompareHighCard(onHandCards23456Dummy, onHandCards34567Dummy));
+            Assert.AreEqual(ResultDual.Lose, 
+                Poker.CompareHighCard(onHandCards23456Dummy, onHandCards34567Dummy));
         }
 
         [Test]
@@ -217,28 +207,19 @@ namespace PokerHandsTest
         }
         #endregion
 
-        [Test]
-        public void OnHandIs_22345_CategoryShouldBe_Pair()
-        {
-            Assert.AreEqual(Poker.Category.Pair, Poker.RecognizeCategory(onHandCards22345Dummy));
-        }
-
-        [Test]
-        public void OnHandIs_23456_CategoryShouldBe_HighCard()
-        {
-            Assert.AreEqual(Poker.Category.HighCard, Poker.RecognizeCategory(onHandCards23456Dummy));
-        }
-
+        #region Pair
         [Test]
         public void ComparePairValue_HandOneIs_22345_And_HandTwoIs_23345_ResultShouldBe_Lose()
         {
-            Assert.AreEqual(ResultDual.Lose, Poker.ComparePair(onHandCards22345Dummy, onHandCards23345Dummy));
+            Assert.AreEqual(ResultDual.Lose, 
+                Poker.ComparePair(onHandCards22345Dummy, onHandCards23345Dummy));
         }
 
         [Test]
         public void ComparePairValue_HandOneIs_23345_And_HandTwoIs_22345_ResultShouldBe_Win()
         {
-            Assert.AreEqual(ResultDual.Win, Poker.ComparePair(onHandCards23345Dummy, onHandCards22345Dummy));
+            Assert.AreEqual(ResultDual.Win, 
+                Poker.ComparePair(onHandCards23345Dummy, onHandCards22345Dummy));
         }
 
         [Test]
@@ -246,17 +227,18 @@ namespace PokerHandsTest
         {
             var onHand22356 = new List<Card>()
             {
-                dummyCardRank2, dummyCardRank2Diamond,
+                dummyCardRank2, dummyCardRank2,
                 dummyCardRank3, dummyCardRank5, dummyCardRank6
             };
 
             var onHand22456 = new List<Card>()
             {
-                dummyCardRank2, dummyCardRank2Diamond,
+                dummyCardRank2, dummyCardRank2,
                 dummyCardRank4, dummyCardRank5, dummyCardRank6
             };
 
             Assert.AreEqual(ResultDual.Lose, Poker.ComparePair(onHand22356, onHand22456));
         }
+        #endregion
     }
 }
