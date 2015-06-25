af8cf91f260a6d6b73e3cc8e356e3a76c800f4c1
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-06-26 04:17:19 +0700

    Refactor

--- PokerHands/Card.cs
+++ PokerHands/Card.cs
@@ -61,11 +61,10 @@ namespace PokerHands
 
         public static bool IsEquals(Card card1, Card card2)
         {
-            if (card1.Suit != card2.Suit)
-                return false;
-            if (card1.Rank != card2.Rank)
-                return false;
-            return true;
+            bool suitIsEqual = card1.Suit == card2.Suit;
+            bool rankIsEqual = card1.Rank == card2.Rank;
+
+            return suitIsEqual && rankIsEqual;
         }
     }
 }
