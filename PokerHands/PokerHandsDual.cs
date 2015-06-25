be1ca9f23bac2a42f95cdcc9128e3a6f7dc93fcd
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-06-26 06:26:17 +0700

    Output should be "Black wins - full house" When Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH

---
+++ PokerHands/PokerHandsDual.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RankType = PokerHands.Card.RankType;
+using ResultDual = PokerHands.Poker.ResultDual;
+using SuitType = PokerHands.Card.SuitType;
+using Category = PokerHands.Poker.Category;
+
+namespace PokerHands
+{
+    public class PokerHandsDual
+    {
+        public static string GetResualDual(List<Card> blackCards, List<Card> whiteCards)
+        {
+            var resultDual = Poker.ComparePokerHands(blackCards, whiteCards);
+            string winnerSide;
+            Category categoryWinner;
+
+            if(resultDual == ResultDual.Win)
+            {
+                winnerSide = "Black";
+                categoryWinner = Poker.RecognizeCategory(blackCards);
+            }
+            else
+            {
+                winnerSide = "White";
+                categoryWinner = Poker.RecognizeCategory(whiteCards);
+            }
+
+            string output = winnerSide + " wins - " + ChangeCategoryForDisplay(categoryWinner);
+            return output;
+        }
+
+        public static string ChangeCategoryForDisplay(Category category)
+        {
+            return "full house";
+        }
+    }
+}
---
+++ PokerHandsTest/PokerHandsDualTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using PokerHands;
+using System;
+using System.Collections.Generic;
+using RankType = PokerHands.Card.RankType;
+using ResultDual = PokerHands.Poker.ResultDual;
+using SuitType = PokerHands.Card.SuitType;
+
+namespace PokerHandsTest
+{
+    [TestFixture]
+    class PokerHandsDualTest
+    {
+        [Test]
+        public void OutputShouldBe_Black_wins_full_house_WhenBlackCardsIs_2H_4S_4C_2D_4H_And_WhiteCardsIs_2S_8S_AS_QS_3S()
+        {
+            string expected = "Black wins - full house";
+
+            var blackCards = Converter.ToCardsList("2H 4S 4C 2D 4H");
+            var whiteCards = Converter.ToCardsList("2S 8S AS QS 3S");
+
+            Assert.AreEqual(expected, PokerHandsDual.GetResualDual(blackCards, whiteCards));
+        }
+    }
+}
