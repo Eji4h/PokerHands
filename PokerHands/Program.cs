0e809229d1a0a9c4bb8b5050ed129f2d1b1f3055
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-06-26 08:41:35 +0700

    Poker hands can play

--- PokerHands/Program.cs
+++ PokerHands/Program.cs
@@ -10,6 +10,32 @@ namespace PokerHands
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string blackCardsInput;
+                string whiteCardsInput;
+
+                Console.Write("Input: " + NewLine +
+                    "Black: ");
+                blackCardsInput = Console.ReadLine();
+
+                Console.Write("White: ");
+                whiteCardsInput = Console.ReadLine();
+
+                var blackCards = Converter.ToCardsList(blackCardsInput);
+                var whiteCards = Converter.ToCardsList(whiteCardsInput);
+
+                Console.WriteLine(PokerHandsDual.GetResultDual(blackCards, whiteCards));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static string NewLine
+        {
+            get { return Environment.NewLine; }
         }
     }
 }
